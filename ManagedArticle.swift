26ef1de5fa23d142d746953dee752b3a4f9c54c3
Sergey Tszyu <sergey.tszyu@gmail.com>
2016-06-24 14:33:06 +0300

    Парсинг и отображение в таблице

---
+++ ManagedArticle.swift
@@ -0,0 +1,17 @@
+//
+//  ManagedArticle.swift
+//  MindStudiosTask
+//
+//  Created by Sergey Tszyu on 22.06.16.
+//  Copyright © 2016 Sergey Tszyu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class ManagedArticle: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
